45cb3dd66f7d1ca8be805956fbd2ae4b0ebd66b6
Roberto Duarte <rbduarte@maisaedu.com.br>
2022-06-12 17:33:03 -0300

    ajustando rota endpoint signup

--- src/routes/_user.js
+++ src/routes/_user.js
@@ -7,7 +7,7 @@ const userRetrieveController = require('../controllers/user/retrieve.controller'
 const userPersistController  = require('../controllers/user/persist.controller');
 
 const init = (expressInstance, basePath) => {
-  expressInstance.post(`${basePath}/auth/signup`, userPersistController.signup);
+  expressInstance.post(`${basePath}/signup`, userPersistController.signup);
   expressInstance.post(`${basePath}/auth/signin`, userRetrieveController.signin);
 };
 
